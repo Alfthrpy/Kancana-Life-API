84a96724fb6950504187f934cc8fbdaa880fb47b
Al-Fathir <118950136+Alfthrpy@users.noreply.github.com>
2025-03-02 22:34:15 +0700

    feat :  refactor tipis tipis

--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -1,33 +1,49 @@
-import authService from './../services/authService.js'
+import authService from "./../services/authService.js";
 
-
-const loginAdmin = async (req,res) =>{
-    const body = req.body
-    console.log(body)
-    try {
-        const result = await authService.loginAdmin(body)
-        return res.status(200).json({accessToken : result.session.access_token})
-    } catch (error) {
-        return res.status(401).json(error.message)
+const loginAdmin = async (req, res) => {
+  const body = req.body;
+  const { email, password } = req.body;
+  if (!email || !password) {
+    return res
+      .status(400)
+      .json({ success: false, message: "Email dan password wajib diisi" });
+  }
+  try {
+    const result = await authService.loginAdmin(body);
+    if (result.success) {
+      return res.status(200).json({ accessToken: result.session.session.access_token });
+    } else {
+      return res.status(401).json({ message: result.message });
     }
-}
+  } catch (error) {
+    return res
+      .status(500)
+      .json({ success: false, message: "Internal server error" });
+  }
+};
 
-const loginUser = async (req,res)=> {
-    const body = req.body
-    try {
-        const result = await authService.loginUser(body)
-        if(result.success){
-            return res.status(200).json({message:result.message,token:result.token})
-        } else {
-            return res.status(401).json({message:result.message})
-        }
-    } catch (error) {
-        console.log(error)
-        return res.status(500).json({message:"Internal server error"})
+const loginUser = async (req, res) => {
+  const body = req.body;
+  if (!req.body.device_id || !req.body.coordinate) {
+    return res
+      .status(400)
+      .json({ message: "Device ID dan koordinat wajib diisi" });
+  }
+  try {
+    const result = await authService.loginUser(body);
+    if (result.success) {
+      return res
+        .status(200)
+        .json({ message: result.message, token: result.token });
+    } else {
+      return res.status(401).json({ message: result.message });
     }
-}
+  } catch (error) {
+    console.log(error);
+    return res.status(500).json({ message: "Internal server error" });
+  }
+};
 
-const controller = {loginAdmin,loginUser}
+const controller = { loginAdmin, loginUser };
 
-export default controller
-
+export default controller;
--- src/services/authService.js
+++ src/services/authService.js
@@ -1,62 +1,74 @@
 import supabase from "../config/database.js";
 import dotenv from "dotenv";
-import jwt from 'jsonwebtoken'
+import jwt from "jsonwebtoken";
 import { reverseGeocode } from "../utils/tools.js";
 dotenv.config();
 
 const loginAdmin = async (form) => {
-    try {
-        const {data,error} = await supabase.auth.signInWithPassword({
-            email:form.email,
-            password:form.password
-        })
-
-        if(error) {
-            throw error
-        }
-
-        return data
-    } catch (error) {
-        throw error
+  try {
+    const { data, error } = await supabase.auth.signInWithPassword({
+      email: form.email,
+      password: form.password,
+    });
+    if (error) {
+      return { success: false, message: "Invalid Credentials" };
     }
-}
+    return { success: true, session: data };
+  } catch (error) {
+    console.error(error);
+    throw error;
+  }
+};
 
-const loginUser = async (form) =>{
-    try {
-        const {data:isDevice,error} = await supabase
-        .from('devices')
-        .select('*')
-        .eq('id',form.device_id)
-        .single()
+const loginUser = async (form) => {
+  try {
+    const { data: isDevice, error } = await supabase
+      .from("devices")
+      .select("*")
+      .eq("id", form.device_id)
+      .single();
 
-        if(!isDevice || error){
-            return {success:false,message:"Device ID tidak ditemukan"}
-        }
-
-        const coordinate = form.coordinate.split(',').map((item)=>parseFloat(item))
-        const location = await reverseGeocode(coordinate[0],coordinate[1])
-
-        if(isDevice.location ===null){
-            const {error:updateError} = await supabase
-            .from('devices')
-            .update({coordinate:form.coordinate,location:location})
-            .eq('id',form.device_id)
-
-            if(updateError){
-                throw updateError
-            }
-        }
-        const token = jwt.sign({ deviceId: form.device_id }, process.env.JWT_SECRET, {
-            expiresIn: "3d", // Token berlaku 7 hari
-        });
-
-        return {success:true,message:"Login berhasil",token}
-        
-    } catch (error) {
-        throw error
+    if (!isDevice) {
+      return { success: false, message: "Device ID tidak ditemukan" };
     }
-}
 
+    if (error) {
+      console.error("Database Error:", error);
+      return { success: false, message: "Terjadi kesalahan server" };
+    }
 
-const service = {loginAdmin,loginUser}
-export default service
+    const coordinate = form.coordinate
+      .split(",")
+      .map((item) => parseFloat(item));
+
+    console.log(coordinate)
+    const location = await reverseGeocode(coordinate[0], coordinate[1]);
+
+    if (isDevice.location === null) {
+      const { error: updateError } = await supabase
+        .from("devices")
+        .update({ coordinate: form.coordinate, location: location ,updated_at: new Date()})
+        .eq("id", form.device_id);
+
+      if (updateError) {
+        return { success: false, message: "Update lokasi gagal" };
+      }
+    }
+
+    const token = jwt.sign(
+      { deviceId: form.device_id },
+      process.env.JWT_SECRET,
+      {
+        expiresIn: "3d",
+      }
+    );
+
+    return { success: true, message: "Login berhasil", token };
+  } catch (error) {
+    console.error(error);
+    throw error;
+  }
+};
+
+const service = { loginAdmin, loginUser };
+export default service;
